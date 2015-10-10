ac23cba4da6a89695a7b1b95eebc3a45b6fc0205
Liam Wall <liam.wall@gmail.com>
2015-10-10 22:06:59 +0100

    Add requirejs build config

---
+++ app.build.js
@@ -0,0 +1,12 @@
+({
+  appDir: ".",
+  baseUrl: "scripts",
+  dir: "../tinyREPL_ghpages/build",
+  modules: [
+      {
+          name: "main"
+      }
+  ],
+  mainConfigFile: 'scripts/main.js',
+  fileExclusionRegExp: /^\./
+})
