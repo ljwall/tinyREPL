0076bd5d5c44be66db52c5c5c003c111bcd98079
Liam Wall <liam.wall@gmail.com>
2015-10-14 08:17:57 +0100

    Additional chnages (should have been with last commit)

--- scripts/CodeModel.js
+++ scripts/CodeModel.js
@@ -1,6 +1,7 @@
 define(['backbone'], function (Backbone) {
   return Backbone.Model.extend({
     defaults: {
+      name: 'default',
       code: ';; This is an editor...\n\n(define foo 42)\n\n'
     }
   });
