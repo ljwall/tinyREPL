bcd45349d61a1970033bdd373fa1198cce6424bf
Liam Wall <liam.wall@gmail.com>
2015-10-16 14:06:31 +0100

    Add 'values' tutorial section

--- scripts/model/sampleData.js
+++ scripts/model/sampleData.js
@@ -1,10 +1,12 @@
 define([
     'model/CodeCollection',
     'text!tinyschemer/intro.scm',
+    'text!tinyschemer/values.scm',
     'text!tinyschemer/functioncalls.scm'
-], function (CodeCollection, intro, functioncalls) {
+], function (CodeCollection, intro, values, functioncalls) {
   return new CodeCollection([
       { name: 'Intro', code: intro },
-      { name: 'Function calls', code:functioncalls }
+      { name: 'Values', code: values },
+      { name: 'Function calls', code: functioncalls }
   ]);
 });
---
+++ scripts/tinyschemer/values.scm
@@ -0,0 +1,14 @@
+;; We have a few basic value types to
+;; play with: Stings, numbers and booleans.
+
+;; Stings are written with double-quotes,
+;; like this:
+;;      "Foo bar!"
+
+;; Numbers are written in decimal, e.g:
+;;      2.718
+
+;; True and false are written as #t and #f.
+
+;; If you type a basic value at the REPL it
+;; will print it back at you. Try it out..
