5e75df0fb8ea6dfbd6a350b7ee51cdb6fd759371
Liam Wall <liam.wall@gmail.com>
2015-10-16 16:06:12 +0100

    Lists and function call tutotorials

--- scripts/model/sampleData.js
+++ scripts/model/sampleData.js
@@ -2,11 +2,13 @@ define([
     'model/CodeCollection',
     'text!tinyschemer/intro.scm',
     'text!tinyschemer/values.scm',
+    'text!tinyschemer/lists.scm',
     'text!tinyschemer/functioncalls.scm'
-], function (CodeCollection, intro, values, functioncalls) {
+], function (CodeCollection, intro, values, lists, functioncalls) {
   return new CodeCollection([
       { name: 'Intro', code: intro },
       { name: 'Values', code: values },
+      { name: 'Lists', code: lists },
       { name: 'Function calls', code: functioncalls }
   ]);
 });
