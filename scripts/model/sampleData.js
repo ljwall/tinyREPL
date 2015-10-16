6f11371b3cc09db54b2bd8cb9058fa76102dff2d
Liam Wall <liam.wall@gmail.com>
2015-10-16 17:54:17 +0100

    Recursion tutorial

--- scripts/model/sampleData.js
+++ scripts/model/sampleData.js
@@ -3,12 +3,18 @@ define([
     'text!tinyschemer/intro.scm',
     'text!tinyschemer/values.scm',
     'text!tinyschemer/lists.scm',
-    'text!tinyschemer/functioncalls.scm'
-], function (CodeCollection, intro, values, lists, functioncalls) {
+    'text!tinyschemer/functioncalls.scm',
+    'text!tinyschemer/listfunctions.scm',
+    'text!tinyschemer/define.scm',
+    'text!tinyschemer/recursionintro.scm'
+], function (CodeCollection, intro, values, lists, functioncalls, listfunctions, define, recursionintro) {
   return new CodeCollection([
       { name: 'Intro', code: intro },
       { name: 'Values', code: values },
       { name: 'Lists', code: lists },
-      { name: 'Function calls', code: functioncalls }
+      { name: 'Function calls', code: functioncalls },
+      { name: 'List functions', code: listfunctions },
+      { name: 'Defining variables and functions', code: define },
+      { name: 'Recursive functions', code: recursionintro}
   ]);
 });
---
+++ scripts/tinyschemer/listfunctions.scm
@@ -0,0 +1,18 @@
+;; There are three functions which are the fundamental
+;; building blocks for working with lists.
+
+;; head - gets the first element of a list:
+;;    (head '(1 2 3 4))
+;;      => 1
+
+;; tail - gets the tail of a list:
+;;    (tail '(1 2 3 4))
+;;      => (2 3 4)
+
+;; cons - constructs a new list from an item and a list:
+;;    (cons 1 '(2 3 4))
+;;      => (1 2 3 4)
+;;
+;; This one is so important it gets a second example!
+;;    (cons "Hello" '())
+;;      => ("Hello")
